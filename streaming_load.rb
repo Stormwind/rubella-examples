2ebc139e699de31002dd49fb1ca83362be026657
Rebecca Dietrich <rebecca.dietrich@1und1.de>
2015-05-06 10:36:55 +0200

    Quickfix

--- streaming_load.rb
+++ streaming_load.rb
@@ -1,10 +1,11 @@
+require "fileutils"
 require "rubella"
 require "rubella/input/base"
 require "rubella/output/image"
 require "rubella/output/ascii"
 require "rubella/weighting/per_value"
 require "rubella/weighting/per_overall_load"
-require "rubella/weighting/expotential"
+require "rubella/weighting/exponential"
 
 # Number of cores
 cores   = 8
@@ -20,7 +21,7 @@ time    = 1
 # Prepare classes
 #weighting = Rubella::Weighting::PerValue.new buckets
 weighting = Rubella::Weighting::PerOverallLoad.new buckets
-#weighting = Rubella::Weighting::Expotential.new buckets
+#weighting = Rubella::Weighting::Exponential.new buckets
 storage   = Rubella::Storage.new Array.new(1, Array.new(cores, 0)), col
 
 while true
@@ -54,8 +55,9 @@ while true
   storage = storage.add new_storage
 
   # Dump new image
-  # Rubella::Output::Image.new(storage, size).render.write("cpu_load.png")
-  puts Rubella::Output::ASCII.new(storage, size).render
+  Rubella::Output::Image.new(storage, size).render.write("cpu_load_new.png")
+  FileUtils.cp "cpu_load_new.png", "cpu_load.png"
+  #puts Rubella::Output::ASCII.new(storage, size).render
 
   sleep(time)
 end
